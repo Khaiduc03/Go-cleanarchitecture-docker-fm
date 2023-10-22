668b3e7d217a6cee77db2372170b3f576c75ce1d
KhaiDuc <132749681+Khaiduc03@users.noreply.github.com>
2023-10-22 13:56:10 +0700

    update docker

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,8 +4,9 @@ check_image_exist() {
     local image_name="mujag/fm-fiber:prod"
     if [ $(sudo docker images -q $image_name) ]; then
        echo "Image already exist"
+        sudo docker push mujag/fm-fiber:prod
     else
-     sudo docker build -t mujag/fm-fiber:prod .
+     sudo docker build -t mujag/fm-fiber:prod -f Dockerfile.prod .
      sudo docker push mujag/fm-fiber:prod
     fi
 }
