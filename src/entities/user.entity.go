0c223393b690f9f1161bb0b5eb886fbe9246c5d3
KhaiDuc <132749681+Khaiduc03@users.noreply.github.com>
2023-10-30 19:21:55 +0700

    format data

--- src/entities/user.entity.go
+++ src/entities/user.entity.go
@@ -20,9 +20,9 @@ type User struct {
 	Email       string `gorm:"column:email;type:varchar(100);unique"`
 	PhoneNumber string `gorm:"column:phone_number;type:varchar(12);default:''"`
 	Url         string `gorm:"column:url;type:varchar(200);default:''"`
-	Role        string `gorm:"column:role;type:varchar(10);default:'USER'"`
-	Position    string `gorm:"column:position;type:varchar(20);default:'HCM'"`
-	Department  string `gorm:"column:department;type:varchar(20);default:'T'"`
+	Role        string `gorm:"column:role;type:varchar(10);default:'TEACHER'"`
+	Position    string `gorm:"column:position;type:varchar(50);default:'FPT Polytechnic Hồ Chí Minh'"`
+	Department  string `gorm:"column:department;type:varchar(20);default:'Toa T'"`
 }
 
 func (User) TableName() string {
